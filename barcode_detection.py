af2333d1c0f1896ff86fb8ff4db5034f04468067
Neha Tandon <57561424+nehat005@users.noreply.github.com>
2022-12-16 16:47:26 +0100

    Add barcode detection sample script

---
+++ barcode_detection.py
@@ -0,0 +1,40 @@
+# https://pyimagesearch.com/2014/11/24/detecting-barcodes-images-python-opencv/
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import imutils
+
+image = cv2.imread('/home/tandon/Desktop/barcode_01.webp')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# compute the Scharr gradient magnitude representation of the images
+# in both the x and y direction using OpenCV 2.4
+ddepth = cv2.CV_32F
+gradX = cv2.Sobel(gray, ddepth=ddepth, dx=1, dy=0, ksize=-1)
+gradY = cv2.Sobel(gray, ddepth=ddepth, dx=0, dy=1, ksize=-1)
+# subtract the y-gradient from the x-gradient
+gradient = cv2.subtract(gradX, gradY)
+plt.imshow(gradient)
+gradient = cv2.convertScaleAbs(gradient)
+blurred = cv2.blur(gradient, (9, 9))
+(_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
+closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+closed = cv2.erode(closed, None, iterations = 4)
+closed = cv2.dilate(closed, None, iterations = 4)
+
+cnts = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+c = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+
+# compute the rotated bounding box of the largest contour
+rect = cv2.minAreaRect(c)
+box = cv2.cv.BoxPoints(rect) if imutils.is_cv2() else cv2.boxPoints(rect)
+box = np.int0(box)
+# draw a bounding box arounded the detected barcode and display the
+# image
+cv2.drawContours(image, [box], -1, (0, 255, 0), 3)
+cv2.imshow("Image", image)
+cv2.waitKey(0)
